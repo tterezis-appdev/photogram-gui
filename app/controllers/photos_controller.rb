ce28ce97f8b60bbf64a9849c208375cdac7ed2f5
Teddy Terezis <tterezis@chicagobooth.edu>
2023-05-02 01:30:38 +0000

    Delete photo complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,24 @@ class PhotosController < ApplicationController
     render ({:template => "photo_templates/index.html.erb"})
   end
 
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_id})
+    @the_photo= matching_photos.at(0)
+    render ({:template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+    the_id=params.fetch("toast_id")
+
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo=matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render ({:template => "photo_templates/baii.html.erb"})
+    
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ get("/users/:path_username", {:controller => "users", :action => "show"})
 
 get("/photos", {:controller => "photos" , :action => "index"})
 
+get("/photos/:path_id", {:controller => "photos", :action => "show"})
+
+get("/delete_photo/:toast_id", {:controller => "photos", :action => "baii"})
 end
